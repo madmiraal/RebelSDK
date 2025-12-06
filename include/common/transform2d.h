4ce3ae509dc2c8be83fab195aba8c09195044043
Marcel Admiraal <madmiraal@users.noreply.github.com>
2025-12-06 16:46:35 +0100

    Clean up Transform2D

--- include/common/transform2d.h
+++ include/common/transform2d.h
@@ -7,120 +7,34 @@
 #ifndef TRANSFORM2D_H
 #define TRANSFORM2D_H
 
+#include "common/defs.h"
 #include "common/vector2.h"
 
 namespace Rebel {
-
+class Rect2;
 typedef Vector2 Size2;
 
-struct Rect2;
+// Note 1: Transform2D basis vectors are stored differently from 3D Basis.
+// Transform2D uses column major column format. The 2D basis matrix looks like:
+//     ┌                             ┐
+//     │elements[0][0] elements[1][0]│
+// M = │elements[0][1] elements[1][1]│
+//     └                             ┘
+// This is the opposite of the indices in mathematical texts.
+// In a math book, M[12] corresponds to Transform2D elements[1][0].
 
-struct Transform2D {
+// Note 2: Unlike 3D code, 2D code uses a left-hand coordinate system.
+// Y-axis points down, and angles are measured clockwise from +X to +Y.
+
+class Transform2D {
+public:
     static const Transform2D IDENTITY;
     static const Transform2D FLIP_X;
     static const Transform2D FLIP_Y;
 
-    // Warning #1: basis of Transform2D is stored differently from Basis. In
-    // terms of elements array, the basis matrix looks like "on paper": M =
-    // (elements[0][0] elements[1][0])
-    //     (elements[0][1] elements[1][1])
-    // This is such that the columns, which can be interpreted as basis vectors
-    // of the coordinate system "painted" on the object, can be accessed as
-    // elements[i]. Note that this is the opposite of the indices in
-    // mathematical texts, meaning: $M_{12}$ in a math book corresponds to
-    // elements[1][0] here. This requires additional care when working with
-    // explicit indices. See
-    // https://en.wikipedia.org/wiki/Row-_and_column-major_order for further
-    // reading.
+    Vector2 elements[3] = {};
 
-    // Warning #2: 2D be aware that unlike 3D code, 2D code uses a left-handed
-    // coordinate system: Y-axis points down, and angle is measure from +X to +Y
-    // in a clockwise-fashion.
-
-    Vector2 elements[3];
-
-    inline real_t tdotx(const Vector2& v) const {
-        return elements[0][0] * v.x + elements[1][0] * v.y;
-    }
-
-    inline real_t tdoty(const Vector2& v) const {
-        return elements[0][1] * v.x + elements[1][1] * v.y;
-    }
-
-    inline const Vector2& operator[](int p_idx) const {
-        return elements[p_idx];
-    }
-
-    inline Vector2& operator[](int p_idx) {
-        return elements[p_idx];
-    }
-
-    inline Vector2 get_axis(int p_axis) const {
-        ERR_FAIL_INDEX_V(p_axis, 3, Vector2());
-        return elements[p_axis];
-    }
-
-    inline void set_axis(int p_axis, const Vector2& p_vec) {
-        ERR_FAIL_INDEX(p_axis, 3);
-        elements[p_axis] = p_vec;
-    }
-
-    void invert();
-    Transform2D inverse() const;
-
-    void affine_invert();
-    Transform2D affine_inverse() const;
-
-    void set_rotation(real_t p_phi);
-    real_t get_rotation() const;
-    void set_rotation_and_scale(real_t p_phi, const Size2& p_scale);
-    void rotate(real_t p_phi);
-
-    void scale(const Size2& p_scale);
-    void scale_basis(const Size2& p_scale);
-    void translate(real_t p_tx, real_t p_ty);
-    void translate(const Vector2& p_translation);
-
-    real_t basis_determinant() const;
-
-    Size2 get_scale() const;
-
-    inline const Vector2& get_origin() const {
-        return elements[2];
-    }
-
-    inline void set_origin(const Vector2& p_origin) {
-        elements[2] = p_origin;
-    }
-
-    Transform2D scaled(const Size2& p_scale) const;
-    Transform2D basis_scaled(const Size2& p_scale) const;
-    Transform2D translated(const Vector2& p_offset) const;
-    Transform2D rotated(real_t p_phi) const;
-
-    Transform2D untranslated() const;
-
-    void orthonormalize();
-    Transform2D orthonormalized() const;
-
-    bool operator==(const Transform2D& p_transform) const;
-    bool operator!=(const Transform2D& p_transform) const;
-
-    void operator*=(const Transform2D& p_transform);
-    Transform2D operator*(const Transform2D& p_transform) const;
-
-    Transform2D interpolate_with(const Transform2D& p_transform, real_t p_c)
-        const;
-
-    Vector2 basis_xform(const Vector2& p_vec) const;
-    Vector2 basis_xform_inv(const Vector2& p_vec) const;
-    Vector2 xform(const Vector2& p_vec) const;
-    Vector2 xform_inv(const Vector2& p_vec) const;
-    Rect2 xform(const Rect2& p_vec) const;
-    Rect2 xform_inv(const Rect2& p_vec) const;
-
-    operator String() const;
-
+    Transform2D() = default;
     Transform2D(
         real_t xx,
         real_t xy,
@@ -129,15 +43,66 @@ struct Transform2D {
         real_t ox,
         real_t oy
     );
+    Transform2D(real_t rotation, const Vector2& position);
 
-    Transform2D(real_t p_rot, const Vector2& p_pos);
+    Vector2& operator[](int index);
+    const Vector2& operator[](int index) const;
 
-    inline Transform2D() {
-        elements[0][0] = 1.0;
-        elements[1][1] = 1.0;
-    }
+    void operator*=(const Transform2D& other);
+
+    operator String() const;
+
+    Vector2 get_axis(int axis) const;
+    void set_axis(int axis, const Vector2& vector2);
+
+    const Vector2& get_origin() const;
+    void set_origin(const Vector2& new_origin);
+
+    void translate(real_t x, real_t y);
+    void translate(const Vector2& translation);
+    Transform2D translated(const Vector2& translation) const;
+    Transform2D untranslated() const;
+
+    real_t get_rotation() const;
+    void set_rotation(real_t radians);
+    void rotate(real_t radians);
+    Transform2D rotated(real_t radians) const;
+
+    Size2 get_scale() const;
+    void scale(const Size2& size);
+    Transform2D scaled(const Size2& size) const;
+    void scale_basis(const Size2& basis_size);
+    Transform2D basis_scaled(const Size2& bais_size) const;
+
+    void set_rotation_and_scale(real_t radians, const Size2& size);
+
+    void orthonormalize();
+    Transform2D orthonormalized() const;
+
+    void invert();
+    Transform2D inverse() const;
+    void affine_invert();
+    Transform2D affine_inverse() const;
+
+    real_t tdotx(const Vector2& vector2) const;
+    real_t tdoty(const Vector2& vector2) const;
+
+    Vector2 xform(const Vector2& vector2) const;
+    Vector2 xform_inv(const Vector2& vector2) const;
+    Vector2 basis_xform(const Vector2& vector2) const;
+    Vector2 basis_xform_inv(const Vector2& vector2) const;
+
+    Rect2 xform(const Rect2& rectangle) const;
+    Rect2 xform_inv(const Rect2& rectangle) const;
+
+    real_t basis_determinant() const;
+
+    Transform2D interpolate_with(const Transform2D& to, real_t factor) const;
 };
 
+Transform2D operator*(Transform2D left, const Transform2D& right);
+bool operator==(const Transform2D& left, const Transform2D& right);
+bool operator!=(const Transform2D& left, const Transform2D& right);
 } // namespace Rebel
 
 #endif // TRANSFORM2D_H
--- src/common/transform2d.cpp
+++ src/common/transform2d.cpp
@@ -10,13 +10,13 @@
 #include "common/string.h"
 #include "common/vector2.h"
 
-#include <algorithm>
+#include <cmath>
+#include <utility>
 
 namespace Rebel {
-
 const Transform2D Transform2D::IDENTITY;
-const Transform2D Transform2D::FLIP_X = Transform2D(-1, 0, 0, 1, 0, 0);
-const Transform2D Transform2D::FLIP_Y = Transform2D(1, 0, 0, -1, 0, 0);
+const Transform2D Transform2D::FLIP_X = Transform2D{-1, 0, 0, 1, 0, 0};
+const Transform2D Transform2D::FLIP_Y = Transform2D{1, 0, 0, -1, 0, 0};
 
 Transform2D::Transform2D(
     real_t xx,
@@ -25,301 +25,34 @@ Transform2D::Transform2D(
     real_t yy,
     real_t ox,
     real_t oy
-) {
-    elements[0][0] = xx;
-    elements[0][1] = xy;
-    elements[1][0] = yx;
-    elements[1][1] = yy;
-    elements[2][0] = ox;
-    elements[2][1] = oy;
+) :
+    elements{
+        {xx, xy},
+        {yx, yy},
+        {ox, oy}
+} {}
+
+Transform2D::Transform2D(const real_t rotation, const Vector2& position) :
+    elements{
+        {std::cos(rotation), std::sin(rotation)},
+        {-std::sin(rotation), std::cos(rotation)},
+        {position}
+} {}
+
+const Vector2& Transform2D::operator[](const int index) const {
+    return elements[index];
 }
 
-Vector2 Transform2D::basis_xform(const Vector2& v) const {
-    return Vector2(tdotx(v), tdoty(v));
+Vector2& Transform2D::operator[](const int index) {
+    return elements[index];
 }
 
-Vector2 Transform2D::basis_xform_inv(const Vector2& v) const {
-    return Vector2(elements[0].dot(v), elements[1].dot(v));
-}
-
-Vector2 Transform2D::xform(const Vector2& v) const {
-    return Vector2(tdotx(v), tdoty(v)) + elements[2];
-}
-
-Vector2 Transform2D::xform_inv(const Vector2& p_vec) const {
-    Vector2 v = p_vec - elements[2];
-
-    return Vector2(elements[0].dot(v), elements[1].dot(v));
-}
-
-Rect2 Transform2D::xform(const Rect2& p_rect) const {
-    Vector2 x        = elements[0] * p_rect.size.x;
-    Vector2 y        = elements[1] * p_rect.size.y;
-    Vector2 position = xform(p_rect.position);
-
-    Rect2 new_rect;
-    new_rect.position = position;
-    new_rect.expand_to(position + x);
-    new_rect.expand_to(position + y);
-    new_rect.expand_to(position + x + y);
-    return new_rect;
-}
-
-void Transform2D::set_rotation_and_scale(real_t p_rot, const Size2& p_scale) {
-    elements[0][0] = ::cos(p_rot) * p_scale.x;
-    elements[1][1] = ::cos(p_rot) * p_scale.y;
-    elements[1][0] = -::sin(p_rot) * p_scale.y;
-    elements[0][1] = ::sin(p_rot) * p_scale.x;
-}
-
-Rect2 Transform2D::xform_inv(const Rect2& p_rect) const {
-    Vector2 ends[4] = {
-        xform_inv(p_rect.position),
-        xform_inv(Vector2(p_rect.position.x, p_rect.position.y + p_rect.size.y)
-        ),
-        xform_inv(Vector2(
-            p_rect.position.x + p_rect.size.x,
-            p_rect.position.y + p_rect.size.y
-        )),
-        xform_inv(Vector2(p_rect.position.x + p_rect.size.x, p_rect.position.y))
-    };
-
-    Rect2 new_rect;
-    new_rect.position = ends[0];
-    new_rect.expand_to(ends[1]);
-    new_rect.expand_to(ends[2]);
-    new_rect.expand_to(ends[3]);
-
-    return new_rect;
-}
-
-void Transform2D::invert() {
-    // FIXME: this function assumes the basis is a rotation matrix, with no
-    // scaling. Transform2D::affine_inverse can handle matrices with scaling, so
-    // GDScript should eventually use that.
-    std::swap(elements[0][1], elements[1][0]);
-    elements[2] = basis_xform(-elements[2]);
-}
-
-Transform2D Transform2D::inverse() const {
-    Transform2D inv = *this;
-    inv.invert();
-    return inv;
-}
-
-void Transform2D::affine_invert() {
-    real_t det = basis_determinant();
-    ERR_FAIL_COND(det == 0);
-    real_t idet = 1.0 / det;
-
-    std::swap(elements[0][0], elements[1][1]);
-    elements[0] *= Vector2(idet, -idet);
-    elements[1] *= Vector2(-idet, idet);
-
-    elements[2] = basis_xform(-elements[2]);
-}
-
-Transform2D Transform2D::affine_inverse() const {
-    Transform2D inv = *this;
-    inv.affine_invert();
-    return inv;
-}
-
-void Transform2D::rotate(real_t p_phi) {
-    *this = Transform2D(p_phi, Vector2()) * (*this);
-}
-
-real_t Transform2D::get_rotation() const {
-    real_t det    = basis_determinant();
-    Transform2D m = orthonormalized();
-    if (det < 0) {
-        m.scale_basis(Size2(-1, -1));
-    }
-    return ::atan2(m[0].y, m[0].x);
-}
-
-void Transform2D::set_rotation(real_t p_rot) {
-    real_t cr      = ::cos(p_rot);
-    real_t sr      = ::sin(p_rot);
-    elements[0][0] = cr;
-    elements[0][1] = sr;
-    elements[1][0] = -sr;
-    elements[1][1] = cr;
-}
-
-Transform2D::Transform2D(real_t p_rot, const Vector2& p_position) {
-    real_t cr      = ::cos(p_rot);
-    real_t sr      = ::sin(p_rot);
-    elements[0][0] = cr;
-    elements[0][1] = sr;
-    elements[1][0] = -sr;
-    elements[1][1] = cr;
-    elements[2]    = p_position;
-}
-
-Size2 Transform2D::get_scale() const {
-    real_t det_sign = basis_determinant() > 0 ? 1 : -1;
-    return det_sign * Size2(elements[0].length(), elements[1].length());
-}
-
-void Transform2D::scale(const Size2& p_scale) {
-    scale_basis(p_scale);
-    elements[2] *= p_scale;
-}
-
-void Transform2D::scale_basis(const Size2& p_scale) {
-    elements[0][0] *= p_scale.x;
-    elements[0][1] *= p_scale.y;
-    elements[1][0] *= p_scale.x;
-    elements[1][1] *= p_scale.y;
-}
-
-void Transform2D::translate(real_t p_tx, real_t p_ty) {
-    translate(Vector2(p_tx, p_ty));
-}
-
-void Transform2D::translate(const Vector2& p_translation) {
-    elements[2] += basis_xform(p_translation);
-}
-
-void Transform2D::orthonormalize() {
-    // Gram-Schmidt Process
-
-    Vector2 x = elements[0];
-    Vector2 y = elements[1];
-
-    x.normalize();
-    y = (y - x * (x.dot(y)));
-    y.normalize();
-
-    elements[0] = x;
-    elements[1] = y;
-}
-
-Transform2D Transform2D::orthonormalized() const {
-    Transform2D on = *this;
-    on.orthonormalize();
-    return on;
-}
-
-bool Transform2D::operator==(const Transform2D& p_transform) const {
-    for (int i = 0; i < 3; i++) {
-        if (elements[i] != p_transform.elements[i]) {
-            return false;
-        }
-    }
-
-    return true;
-}
-
-bool Transform2D::operator!=(const Transform2D& p_transform) const {
-    for (int i = 0; i < 3; i++) {
-        if (elements[i] != p_transform.elements[i]) {
-            return true;
-        }
-    }
-
-    return false;
-}
-
-void Transform2D::operator*=(const Transform2D& p_transform) {
-    elements[2] = xform(p_transform.elements[2]);
-
-    real_t x0, x1, y0, y1;
-
-    x0 = tdotx(p_transform.elements[0]);
-    x1 = tdoty(p_transform.elements[0]);
-    y0 = tdotx(p_transform.elements[1]);
-    y1 = tdoty(p_transform.elements[1]);
-
-    elements[0][0] = x0;
-    elements[0][1] = x1;
-    elements[1][0] = y0;
-    elements[1][1] = y1;
-}
-
-Transform2D Transform2D::operator*(const Transform2D& p_transform) const {
-    Transform2D t  = *this;
-    t             *= p_transform;
-    return t;
-}
-
-Transform2D Transform2D::scaled(const Size2& p_scale) const {
-    Transform2D copy = *this;
-    copy.scale(p_scale);
-    return copy;
-}
-
-Transform2D Transform2D::basis_scaled(const Size2& p_scale) const {
-    Transform2D copy = *this;
-    copy.scale_basis(p_scale);
-    return copy;
-}
-
-Transform2D Transform2D::untranslated() const {
-    Transform2D copy = *this;
-    copy.elements[2] = Vector2();
-    return copy;
-}
-
-Transform2D Transform2D::translated(const Vector2& p_offset) const {
-    Transform2D copy = *this;
-    copy.translate(p_offset);
-    return copy;
-}
-
-Transform2D Transform2D::rotated(real_t p_phi) const {
-    Transform2D copy = *this;
-    copy.rotate(p_phi);
-    return copy;
-}
-
-real_t Transform2D::basis_determinant() const {
-    return elements[0].x * elements[1].y - elements[0].y * elements[1].x;
-}
-
-Transform2D Transform2D::interpolate_with(
-    const Transform2D& p_transform,
-    real_t p_c
-) const {
-    // extract parameters
-    Vector2 p1 = get_origin();
-    Vector2 p2 = p_transform.get_origin();
-
-    real_t r1 = get_rotation();
-    real_t r2 = p_transform.get_rotation();
-
-    Size2 s1 = get_scale();
-    Size2 s2 = p_transform.get_scale();
-
-    // slerp rotation
-    Vector2 v1(::cos(r1), ::sin(r1));
-    Vector2 v2(::cos(r2), ::sin(r2));
-
-    real_t dot = v1.dot(v2);
-
-    dot = (dot < -1.0) ? -1.0 : ((dot > 1.0) ? 1.0 : dot); // clamp dot to
-                                                           // [-1,1]
-
-    Vector2 v;
-
-    if (dot > 0.9995) {
-        v = Vector2::linear_interpolate(v1, v2, p_c)
-                .normalized(
-                ); // linearly interpolate to avoid numerical precision issues
-    } else {
-        real_t angle = p_c * ::acos(dot);
-        Vector2 v3   = (v2 - v1 * dot).normalized();
-        v            = v1 * ::cos(angle) + v3 * ::sin(angle);
-    }
-
-    // construct matrix
-    Transform2D res(
-        ::atan2(v.y, v.x),
-        Vector2::linear_interpolate(p1, p2, p_c)
-    );
-    res.scale_basis(Vector2::linear_interpolate(s1, s2, p_c));
-    return res;
+void Transform2D::operator*=(const Transform2D& other) {
+    elements[0][0] = tdotx(other.elements[0]);
+    elements[0][1] = tdoty(other.elements[0]);
+    elements[1][0] = tdotx(other.elements[1]);
+    elements[1][1] = tdoty(other.elements[1]);
+    elements[2]    = xform(other.elements[2]);
 }
 
 Transform2D::operator String() const {
@@ -328,4 +61,263 @@ Transform2D::operator String() const {
     );
 }
 
+Vector2 Transform2D::get_axis(const int axis) const {
+    ERR_FAIL_INDEX_V(axis, 3, Vector2());
+    return elements[axis];
+}
+
+void Transform2D::set_axis(const int axis, const Vector2& vector2) {
+    ERR_FAIL_INDEX(axis, 3);
+    elements[axis] = vector2;
+}
+
+const Vector2& Transform2D::get_origin() const {
+    return elements[2];
+}
+
+void Transform2D::set_origin(const Vector2& new_origin) {
+    elements[2] = new_origin;
+}
+
+void Transform2D::translate(const real_t x, const real_t y) {
+    translate(Vector2(x, y));
+}
+
+void Transform2D::translate(const Vector2& translation) {
+    elements[2] += basis_xform(translation);
+}
+
+Transform2D Transform2D::translated(const Vector2& translation) const {
+    Transform2D result = *this;
+    result.translate(translation);
+    return result;
+}
+
+Transform2D Transform2D::untranslated() const {
+    Transform2D result = *this;
+    result.elements[2] = Vector2();
+    return result;
+}
+
+real_t Transform2D::get_rotation() const {
+    Transform2D transform = orthonormalized();
+    if (basis_determinant() < 0) {
+        transform.scale_basis(Size2(-1, -1));
+    }
+    return std::atan2(transform[0].y, transform[0].x);
+}
+
+void Transform2D::set_rotation(const real_t radians) {
+    elements[0][0] = std::cos(radians);
+    elements[0][1] = std::sin(radians);
+    elements[1][0] = -std::sin(radians);
+    elements[1][1] = std::cos(radians);
+}
+
+void Transform2D::rotate(const real_t radians) {
+    *this = rotated(radians);
+}
+
+Transform2D Transform2D::rotated(const real_t radians) const {
+    return Transform2D(radians, Vector2()) * *this;
+}
+
+Size2 Transform2D::get_scale() const {
+    const real_t sign = basis_determinant() > 0 ? 1 : -1;
+    return sign * Size2(elements[0].length(), elements[1].length());
+}
+
+void Transform2D::scale(const Size2& size) {
+    scale_basis(size);
+    elements[2] *= size;
+}
+
+Transform2D Transform2D::scaled(const Size2& size) const {
+    Transform2D result = *this;
+    result.scale(size);
+    return result;
+}
+
+void Transform2D::scale_basis(const Size2& basis_size) {
+    elements[0][0] *= basis_size.x;
+    elements[0][1] *= basis_size.y;
+    elements[1][0] *= basis_size.x;
+    elements[1][1] *= basis_size.y;
+}
+
+Transform2D Transform2D::basis_scaled(const Size2& bais_size) const {
+    Transform2D result = *this;
+    result.scale_basis(bais_size);
+    return result;
+}
+
+void Transform2D::set_rotation_and_scale(
+    const real_t radians,
+    const Size2& size
+) {
+    elements[0][0] = std::cos(radians) * size.x;
+    elements[0][1] = std::sin(radians) * size.x;
+    elements[1][0] = -std::sin(radians) * size.y;
+    elements[1][1] = std::cos(radians) * size.y;
+}
+
+void Transform2D::orthonormalize() {
+    // Gram-Schmidt Process
+    Vector2 x = elements[0];
+    Vector2 y = elements[1];
+    x.normalize();
+    y = y - x * x.dot(y);
+    y.normalize();
+    elements[0] = x;
+    elements[1] = y;
+}
+
+Transform2D Transform2D::orthonormalized() const {
+    Transform2D result = *this;
+    result.orthonormalize();
+    return result;
+}
+
+void Transform2D::invert() {
+    // FIXME: We are assuming the basis is a rotation matrix, with no scaling.
+    // Transform2D::affine_inverse can handle matrices with scaling.
+    std::swap(elements[0][1], elements[1][0]);
+    elements[2] = basis_xform(-elements[2]);
+}
+
+Transform2D Transform2D::inverse() const {
+    Transform2D result = *this;
+    result.invert();
+    return result;
+}
+
+void Transform2D::affine_invert() {
+    const real_t determinant = basis_determinant();
+    ERR_FAIL_COND(determinant == 0);
+    const real_t determinant_inverse = 1 / determinant;
+    std::swap(elements[0][0], elements[1][1]);
+    elements[0] *= Vector2(determinant_inverse, -determinant_inverse);
+    elements[1] *= Vector2(-determinant_inverse, determinant_inverse);
+    elements[2]  = basis_xform(-elements[2]);
+}
+
+Transform2D Transform2D::affine_inverse() const {
+    Transform2D result = *this;
+    result.affine_invert();
+    return result;
+}
+
+real_t Transform2D::tdotx(const Vector2& vector2) const {
+    return elements[0][0] * vector2.x + elements[1][0] * vector2.y;
+}
+
+real_t Transform2D::tdoty(const Vector2& vector2) const {
+    return elements[0][1] * vector2.x + elements[1][1] * vector2.y;
+}
+
+Vector2 Transform2D::xform(const Vector2& vector2) const {
+    return Vector2(tdotx(vector2), tdoty(vector2)) + elements[2];
+}
+
+Vector2 Transform2D::xform_inv(const Vector2& vector2) const {
+    const Vector2 translation = vector2 - elements[2];
+    return {elements[0].dot(translation), elements[1].dot(translation)};
+}
+
+Vector2 Transform2D::basis_xform(const Vector2& vector2) const {
+    return {tdotx(vector2), tdoty(vector2)};
+}
+
+Vector2 Transform2D::basis_xform_inv(const Vector2& vector2) const {
+    return {elements[0].dot(vector2), elements[1].dot(vector2)};
+}
+
+Rect2 Transform2D::xform(const Rect2& rectangle) const {
+    const Vector2 x        = elements[0] * rectangle.size.x;
+    const Vector2 y        = elements[1] * rectangle.size.y;
+    const Vector2 position = xform(rectangle.position);
+    Rect2 result;
+    result.position = position;
+    result.expand_to(position + x);
+    result.expand_to(position + y);
+    result.expand_to(position + x + y);
+    return result;
+}
+
+Rect2 Transform2D::xform_inv(const Rect2& rectangle) const {
+    Rect2 result;
+    result.position = xform_inv(rectangle.position);
+    result.expand_to(xform_inv(
+        Vector2(rectangle.position.x, rectangle.position.y + rectangle.size.y)
+    ));
+    result.expand_to(xform_inv(Vector2(
+        rectangle.position.x + rectangle.size.x,
+        rectangle.position.y + rectangle.size.y
+    )));
+    result.expand_to(xform_inv(
+        Vector2(rectangle.position.x + rectangle.size.x, rectangle.position.y)
+    ));
+    return result;
+}
+
+real_t Transform2D::basis_determinant() const {
+    return elements[0].x * elements[1].y - elements[0].y * elements[1].x;
+}
+
+Transform2D Transform2D::interpolate_with(
+    const Transform2D& to,
+    const real_t factor
+) const {
+    const Vector2 from_position = get_origin();
+    const Vector2 to_position   = to.get_origin();
+    const real_t from_radians   = get_rotation();
+    const real_t to_radians     = to.get_rotation();
+    const Size2 from_size       = get_scale();
+    const Size2 to_size         = to.get_scale();
+    // Slerp rotation.
+    const Vector2 from_rotation(std::cos(from_radians), std::sin(from_radians));
+    const Vector2 to_rotation(std::cos(to_radians), std::sin(to_radians));
+    real_t dot = from_rotation.dot(to_rotation);
+    // Clamp dot [-1, 1].
+    if (dot < -1) {
+        dot = -1;
+    } else if (dot > 1.0) {
+        dot = 1;
+    }
+    Vector2 rotation;
+    if (dot > 0.9995) {
+        // Use linear interpolation to avoid numerical precision issues.
+        rotation =
+            from_rotation.linear_interpolate(to_rotation, factor).normalized();
+    } else {
+        const real_t angle = factor * std::acos(dot);
+        rotation           = from_rotation * std::cos(angle)
+                 + (to_rotation - from_rotation * dot).normalized()
+                       * std::sin(angle);
+    }
+    Transform2D result(
+        std::atan2(rotation.y, rotation.x),
+        Vector2::linear_interpolate(from_position, to_position, factor)
+    );
+    result.scale_basis(Vector2::linear_interpolate(from_size, to_size, factor));
+    return result;
+}
+
+Transform2D operator*(Transform2D left, const Transform2D& right) {
+    left *= right;
+    return left;
+}
+
+bool operator==(const Transform2D& left, const Transform2D& right) {
+    for (int i = 0; i < 3; i++) {
+        if (left.elements[i] != right.elements[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool operator!=(const Transform2D& left, const Transform2D& right) {
+    return !(left == right);
+}
 } // namespace Rebel
