b7d9ea31d5f112208daab0eae4c205aa192a2b3c
Marcel Admiraal <madmiraal@users.noreply.github.com>
2025-12-13 17:06:06 +0100

    Clean up Ref

--- include/common/ref.h
+++ include/common/ref.h
@@ -7,202 +7,162 @@
 #ifndef REF_H
 #define REF_H
 
-#include "classes/object.h"
 #include "common/rebelglobal.h"
 #include "common/variant.h"
 
 namespace Rebel {
 class Reference;
 
-// Replicates Rebel's Ref<T> behavior
 template <class T>
 class Ref {
-    // TODO For this nice check to work, each class must actually #include
-    // Reference classes mentionned in its methods, which might be annoying for
-    // coders who prefer to forward-declare to reduce compile times
-    // static_assert(std::is_base_of<Reference, T>::value,
-    // 		"Ref<T> can only be used with classes deriving from Reference");
-
-    T* reference = nullptr;
-
-    void ref(const Ref& p_from) {
-        if (p_from.reference == reference) {
-            return;
-        }
-
-        unref();
-
-        reference = p_from.reference;
-        if (reference) {
-            reference->reference();
-        }
-    }
-
-    void ref_pointer(T* p_ref) {
-        ERR_FAIL_COND(p_ref == nullptr);
-
-        if (p_ref->init_ref()) {
-            reference = p_ref;
-        }
-    }
-
 public:
-    inline bool operator<(const Ref<T>& p_r) const {
-        return reference < p_r.reference;
+    Ref() = default;
+
+    Ref(T* new_object) {
+        if (new_object) {
+            init_ref(new_object);
+        }
     }
 
-    inline bool operator==(const Ref<T>& p_r) const {
-        return reference == p_r.reference;
-    }
-
-    inline bool operator!=(const Ref<T>& p_r) const {
-        return reference != p_r.reference;
-    }
-
-    inline T* operator->() {
-        return reference;
-    }
-
-    inline T* operator*() {
-        return reference;
-    }
-
-    inline const T* operator->() const {
-        return reference;
-    }
-
-    inline const T* ptr() const {
-        return reference;
-    }
-
-    inline T* ptr() {
-        return reference;
-    }
-
-    inline const T* operator*() const {
-        return reference;
-    }
-
-    operator Variant() const {
-        // Note: the C API handles the cases where the object is a Reference,
-        // so the Variant will be correctly constructed with a RefPtr
-        // engine-side
-        return Variant((Object*)reference);
-    }
-
-    void operator=(const Ref& p_from) {
-        ref(p_from);
+    Ref(const Ref& other) {
+        *this = other;
     }
 
     template <class T_Other>
-    void operator=(const Ref<T_Other>& p_from) {
-        Reference* refb =
-            const_cast<Reference*>(static_cast<const Reference*>(p_from.ptr()));
-        if (refb == nullptr) {
-            unref();
-            return;
-        }
-        Ref r;
-        r.reference = Object::cast_to<T>(refb);
-        ref(r);
-        r.reference = nullptr;
+    Ref(const Ref<T_Other>& other) {
+        *this = other;
     }
 
-    void operator=(const Variant& p_variant) {
-        Object* refb = T::from_variant(p_variant);
-        if (refb == nullptr) {
-            unref();
-            return;
-        }
-        Ref r;
-        r.reference = Object::cast_to<T>(refb);
-        ref(r);
-        r.reference = nullptr;
-    }
-
-    Ref(const Ref& p_from) {
-        reference = nullptr;
-        ref(p_from);
-    }
-
-    template <class T_Other>
-    Ref(const Ref<T_Other>& p_from) {
-        reference = nullptr;
-        Reference* refb =
-            const_cast<Reference*>(static_cast<const Reference*>(p_from.ptr()));
-        if (refb == nullptr) {
-            unref();
-            return;
-        }
-        Ref r;
-        r.reference = Object::cast_to<T>(refb);
-        ref(r);
-        r.reference = nullptr;
-    }
-
-    Ref(T* p_reference) {
-        if (p_reference) {
-            ref_pointer(p_reference);
-        } else {
-            reference = nullptr;
-        }
-    }
-
-    Ref(const Variant& p_variant) {
-        reference    = nullptr;
-        Object* refb = T::from_variant(p_variant);
-        if (refb == nullptr) {
-            unref();
-            return;
-        }
-        Ref r;
-        r.reference = Object::cast_to<T>(refb);
-        ref(r);
-        r.reference = nullptr;
-    }
-
-    inline bool is_valid() const {
-        return reference != nullptr;
-    }
-
-    inline bool is_null() const {
-        return reference == nullptr;
-    }
-
-    void unref() {
-        // TODO this should be moved to mutexes, since this engine does not
-        // really
-        //  do a lot of referencing on references and stuff
-        //  mutexes will avoid more crashes?
-
-        if (reference && reference->unreference()) {
-            // memdelete(reference);
-            reference->free();
-        }
-        reference = nullptr;
-    }
-
-    void instance() {
-        // ref(memnew(T));
-        ref_pointer(T::create());
-    }
-
-    Ref() {
-        reference = nullptr;
+    Ref(const Variant& variant) {
+        *this = variant;
     }
 
     ~Ref() {
         unref();
     }
 
+    void operator=(const Ref& other) {
+        set_ref(other.object);
+    }
+
+    template <class T_Other>
+    void operator=(const Ref<T_Other>& other) {
+        set_ref(other.object);
+    }
+
+    void operator=(const Variant& variant) {
+        const Object* other_object = T::from_variant(variant);
+        set_ref(other_object);
+    }
+
+    T* operator->() {
+        return object;
+    }
+
+    const T* operator->() const {
+        return object;
+    }
+
+    T* operator*() {
+        return object;
+    }
+
+    const T* operator*() const {
+        return object;
+    }
+
+    T* ptr() {
+        return object;
+    }
+
+    const T* ptr() const {
+        return object;
+    }
+
+    operator Variant() const {
+        // Note: the C API handles the cases where the object is a Reference,
+        // so Rebel Engine will correctly construct the Variant with a RefPtr.
+        return Variant(object);
+    }
+
+    bool is_valid() const {
+        return object != nullptr;
+    }
+
+    bool is_null() const {
+        return object == nullptr;
+    }
+
+    void unref() {
+        // TODO Should this be protected with mutexes?
+        if (object && object->unreference()) {
+            object->free();
+        }
+        object = nullptr;
+    }
+
+    void instance() {
+        init_ref(T::create());
+    }
+
     // Used to encapsulate return values without adding to the refcount.
-    inline static Ref<T> create_ref(Object* object) {
-        Ref<T> ref;
-        ref.reference = (T*)object;
-        return ref;
+    static Ref create_ref(Object* object) {
+        Ref result;
+        result.object = static_cast<T*>(object);
+        return result;
+    }
+
+private:
+    T* object = nullptr;
+
+    void set_ref(T* other_object) {
+        if (object == other_object) {
+            return;
+        }
+        unref();
+        object = other_object;
+        if (object) {
+            object->reference();
+        }
+    }
+
+    void init_ref(T* new_object) {
+        ERR_FAIL_COND(new_object == nullptr);
+        if (new_object->init_ref()) {
+            object = new_object;
+        }
     }
 };
 
+template <class T>
+bool operator==(const Ref<T>& left, const Ref<T>& right) {
+    return left.object == right.object;
+}
+
+template <class T>
+bool operator!=(const Ref<T>& left, const Ref<T>& right) {
+    return !(left == right);
+}
+
+template <class T>
+bool operator<(const Ref<T>& left, const Ref<T>& right) {
+    return left.object < right.object;
+}
+
+template <class T>
+bool operator<=(const Ref<T>& left, const Ref<T>& right) {
+    return left == right || left < right;
+}
+
+template <class T>
+bool operator>(const Ref<T>& left, const Ref<T>& right) {
+    return !(left <= right);
+}
+
+template <class T>
+bool operator>=(const Ref<T>& left, const Ref<T>& right) {
+    return !(left < right);
+}
 } // namespace Rebel
 
 #endif // REF_H
