a5f09af92132f8506e9edcef75ec53a01dc19063
Marcel Admiraal <madmiraal@users.noreply.github.com>
2025-12-13 10:53:17 +0100

    Clean up Transform

--- include/common/transform.h
+++ include/common/transform.h
@@ -12,7 +12,6 @@
 #include "common/plane.h"
 
 namespace Rebel {
-
 class Transform {
 public:
     static const Transform IDENTITY;
@@ -23,77 +22,31 @@ public:
     Basis basis;
     Vector3 origin;
 
-    void invert();
-    Transform inverse() const;
-
-    void affine_invert();
-    Transform affine_inverse() const;
-
-    Transform rotated(const Vector3& p_axis, real_t p_phi) const;
-
-    void rotate(const Vector3& p_axis, real_t p_phi);
-    void rotate_basis(const Vector3& p_axis, real_t p_phi);
-
-    void set_look_at(
-        const Vector3& p_eye,
-        const Vector3& p_target,
-        const Vector3& p_up
+    Transform() = default;
+    Transform(const Basis& basis, const Vector3& origin = Vector3());
+    Transform(
+        real_t xx,
+        real_t xy,
+        real_t xz,
+        real_t yx,
+        real_t yy,
+        real_t yz,
+        real_t zx,
+        real_t zy,
+        real_t zz,
+        real_t tx,
+        real_t ty,
+        real_t tz
     );
-    Transform looking_at(const Vector3& p_target, const Vector3& p_up) const;
-
-    void scale(const Vector3& p_scale);
-    Transform scaled(const Vector3& p_scale) const;
-    void scale_basis(const Vector3& p_scale);
-    void translate(real_t p_tx, real_t p_ty, real_t p_tz);
-    void translate(const Vector3& p_translation);
-    Transform translated(const Vector3& p_translation) const;
-
-    inline const Basis& get_basis() const {
-        return basis;
-    }
-
-    inline void set_basis(const Basis& p_basis) {
-        basis = p_basis;
-    }
-
-    inline const Vector3& get_origin() const {
-        return origin;
-    }
-
-    inline void set_origin(const Vector3& p_origin) {
-        origin = p_origin;
-    }
-
-    void orthonormalize();
-    Transform orthonormalized() const;
-
-    bool operator==(const Transform& p_transform) const;
-    bool operator!=(const Transform& p_transform) const;
-
-    Vector3 xform(const Vector3& p_vector) const;
-    Vector3 xform_inv(const Vector3& p_vector) const;
-
-    Plane xform(const Plane& p_plane) const;
-    Plane xform_inv(const Plane& p_plane) const;
-
-    AABB xform(const AABB& p_aabb) const;
-    AABB xform_inv(const AABB& p_aabb) const;
 
     void operator*=(const Transform& p_transform);
-    Transform operator*(const Transform& p_transform) const;
 
-    inline Vector3 operator*(const Vector3& p_vector) const {
-        return Vector3(
-            basis.elements[0].dot(p_vector) + origin.x,
-            basis.elements[1].dot(p_vector) + origin.y,
-            basis.elements[2].dot(p_vector) + origin.z
-        );
-    }
+    operator String() const;
 
-    Transform interpolate_with(const Transform& p_transform, real_t p_c) const;
-
-    Transform inverse_xform(const Transform& t) const;
-
+    const Basis& get_basis() const;
+    void set_basis(const Basis& p_basis);
+    const Vector3& get_origin() const;
+    void set_origin(const Vector3& p_origin);
     void set(
         real_t xx,
         real_t xy,
@@ -109,30 +62,49 @@ public:
         real_t tz
     );
 
-    operator String() const;
+    void translate(const Vector3& translation);
+    void translate(real_t x, real_t y, real_t z);
+    Transform translated(const Vector3& translation) const;
 
-    inline Transform(
-        real_t xx,
-        real_t xy,
-        real_t xz,
-        real_t yx,
-        real_t yy,
-        real_t yz,
-        real_t zx,
-        real_t zy,
-        real_t zz,
-        real_t tx,
-        real_t ty,
-        real_t tz
-    ) {
-        set(xx, xy, xz, yx, yy, yz, zx, zy, zz, tx, ty, tz);
-    }
+    void rotate(const Vector3& axis, real_t radians);
+    Transform rotated(const Vector3& axis, real_t radians) const;
+    void rotate_basis(const Vector3& axis, real_t radians);
 
-    Transform(const Basis& p_basis, const Vector3& p_origin = Vector3());
+    void scale(const Vector3& factor);
+    Transform scaled(const Vector3& factor) const;
+    void scale_basis(const Vector3& factor);
 
-    inline Transform() {}
+    Transform looking_at(const Vector3& target, const Vector3& up) const;
+    void set_look_at(
+        const Vector3& eye,
+        const Vector3& target,
+        const Vector3& up
+    );
+
+    void invert();
+    Transform inverse() const;
+    void affine_invert();
+    Transform affine_inverse() const;
+
+    void orthonormalize();
+    Transform orthonormalized() const;
+
+    Vector3 xform(const Vector3& vector3) const;
+    Vector3 xform_inv(const Vector3& vector3) const;
+    Plane xform(const Plane& plane) const;
+    Plane xform_inv(const Plane& plane) const;
+    AABB xform(const AABB& aabb) const;
+    AABB xform_inv(const AABB& aabb) const;
+
+    Transform inverse_xform(const Transform& other) const;
+
+    Transform interpolate_with(const Transform& to, real_t factor) const;
 };
 
+bool operator==(const Transform& left, const Transform& right);
+bool operator!=(const Transform& left, const Transform& right);
+Transform operator*(const Transform& left, const Transform& right);
+Vector3 operator*(const Transform& transform, const Vector3& vector3);
 } // namespace Rebel
 
 #endif // TRANSFORM_H
--- src/common/transform.cpp
+++ src/common/transform.cpp
@@ -21,155 +21,142 @@ const Transform Transform::FLIP_Y =
 const Transform Transform::FLIP_Z =
     Transform(1, 0, 0, 0, 1, 0, 0, 0, -1, 0, 0, 0);
 
-Transform Transform::inverse_xform(const Transform& t) const {
-    Vector3 v = t.origin - origin;
-    return Transform(basis.transpose_xform(t.basis), basis.xform(v));
+Transform::Transform(const Basis& basis, const Vector3& origin) :
+    basis(basis),
+    origin(origin) {}
+
+Transform::Transform(
+    const real_t xx,
+    const real_t xy,
+    const real_t xz,
+    const real_t yx,
+    const real_t yy,
+    const real_t yz,
+    const real_t zx,
+    const real_t zy,
+    const real_t zz,
+    const real_t tx,
+    const real_t ty,
+    const real_t tz
+) :
+    basis{xx, xy, xz, yx, yy, yz, zx, zy, zz},
+    origin{tx, ty, tz} {}
+
+void Transform::operator*=(const Transform& p_transform) {
+    origin  = xform(p_transform.origin);
+    basis  *= p_transform.basis;
+}
+
+Transform::operator String() const {
+    return basis.operator String() + " - " + origin.operator String();
+}
+
+const Basis& Transform::get_basis() const {
+    return basis;
+}
+
+void Transform::set_basis(const Basis& p_basis) {
+    basis = p_basis;
+}
+
+const Vector3& Transform::get_origin() const {
+    return origin;
+}
+
+void Transform::set_origin(const Vector3& p_origin) {
+    origin = p_origin;
 }
 
 void Transform::set(
-    real_t xx,
-    real_t xy,
-    real_t xz,
-    real_t yx,
-    real_t yy,
-    real_t yz,
-    real_t zx,
-    real_t zy,
-    real_t zz,
-    real_t tx,
-    real_t ty,
-    real_t tz
+    const real_t xx,
+    const real_t xy,
+    const real_t xz,
+    const real_t yx,
+    const real_t yy,
+    const real_t yz,
+    const real_t zx,
+    const real_t zy,
+    const real_t zz,
+    const real_t tx,
+    const real_t ty,
+    const real_t tz
 ) {
-    basis.elements[0][0] = xx;
-    basis.elements[0][1] = xy;
-    basis.elements[0][2] = xz;
-    basis.elements[1][0] = yx;
-    basis.elements[1][1] = yy;
-    basis.elements[1][2] = yz;
-    basis.elements[2][0] = zx;
-    basis.elements[2][1] = zy;
-    basis.elements[2][2] = zz;
-    origin.x             = tx;
-    origin.y             = ty;
-    origin.z             = tz;
+    basis  = {xx, xy, xz, yx, yy, yz, zx, zy, zz};
+    origin = {tx, ty, tz};
 }
 
-Vector3 Transform::xform(const Vector3& p_vector) const {
-    return Vector3(
-        basis.elements[0].dot(p_vector) + origin.x,
-        basis.elements[1].dot(p_vector) + origin.y,
-        basis.elements[2].dot(p_vector) + origin.z
-    );
-}
-
-Vector3 Transform::xform_inv(const Vector3& p_vector) const {
-    Vector3 v = p_vector - origin;
-
-    return Vector3(
-        (basis.elements[0][0] * v.x) + (basis.elements[1][0] * v.y)
-            + (basis.elements[2][0] * v.z),
-        (basis.elements[0][1] * v.x) + (basis.elements[1][1] * v.y)
-            + (basis.elements[2][1] * v.z),
-        (basis.elements[0][2] * v.x) + (basis.elements[1][2] * v.y)
-            + (basis.elements[2][2] * v.z)
-    );
-}
-
-Plane Transform::xform(const Plane& p_plane) const {
-    Vector3 point     = p_plane.normal * p_plane.d;
-    Vector3 point_dir = point + p_plane.normal;
-    point             = xform(point);
-    point_dir         = xform(point_dir);
-
-    Vector3 normal = point_dir - point;
-    normal.normalize();
-    real_t d = normal.dot(point);
-
-    return Plane(normal, d);
-}
-
-Plane Transform::xform_inv(const Plane& p_plane) const {
-    Vector3 point     = p_plane.normal * p_plane.d;
-    Vector3 point_dir = point + p_plane.normal;
-    point             = xform_inv(point);
-    point_dir         = xform_inv(point_dir);
-
-    Vector3 normal = point_dir - point;
-    normal.normalize();
-    real_t d = normal.dot(point);
-
-    return Plane(normal, d);
-}
-
-AABB Transform::xform(const AABB& p_aabb) const {
-    /* define vertices */
-    Vector3 x   = basis.get_axis(0) * p_aabb.size.x;
-    Vector3 y   = basis.get_axis(1) * p_aabb.size.y;
-    Vector3 z   = basis.get_axis(2) * p_aabb.size.z;
-    Vector3 pos = xform(p_aabb.position);
-    // could be even further optimized
-    AABB new_aabb;
-    new_aabb.position = pos;
-    new_aabb.expand_to(pos + x);
-    new_aabb.expand_to(pos + y);
-    new_aabb.expand_to(pos + z);
-    new_aabb.expand_to(pos + x + y);
-    new_aabb.expand_to(pos + x + z);
-    new_aabb.expand_to(pos + y + z);
-    new_aabb.expand_to(pos + x + y + z);
-    return new_aabb;
-}
-
-AABB Transform::xform_inv(const AABB& p_aabb) const {
-    /* define vertices */
-    Vector3 vertices[8] = {
-        Vector3(
-            p_aabb.position.x + p_aabb.size.x,
-            p_aabb.position.y + p_aabb.size.y,
-            p_aabb.position.z + p_aabb.size.z
-        ),
-        Vector3(
-            p_aabb.position.x + p_aabb.size.x,
-            p_aabb.position.y + p_aabb.size.y,
-            p_aabb.position.z
-        ),
-        Vector3(
-            p_aabb.position.x + p_aabb.size.x,
-            p_aabb.position.y,
-            p_aabb.position.z + p_aabb.size.z
-        ),
-        Vector3(
-            p_aabb.position.x + p_aabb.size.x,
-            p_aabb.position.y,
-            p_aabb.position.z
-        ),
-        Vector3(
-            p_aabb.position.x,
-            p_aabb.position.y + p_aabb.size.y,
-            p_aabb.position.z + p_aabb.size.z
-        ),
-        Vector3(
-            p_aabb.position.x,
-            p_aabb.position.y + p_aabb.size.y,
-            p_aabb.position.z
-        ),
-        Vector3(
-            p_aabb.position.x,
-            p_aabb.position.y,
-            p_aabb.position.z + p_aabb.size.z
-        ),
-        Vector3(p_aabb.position.x, p_aabb.position.y, p_aabb.position.z)
-    };
-
-    AABB ret;
-
-    ret.position = xform_inv(vertices[0]);
-
-    for (int i = 1; i < 8; i++) {
-        ret.expand_to(xform_inv(vertices[i]));
+void Transform::translate(const Vector3& translation) {
+    for (int i = 0; i < 3; i++) {
+        origin[i] += basis.elements[i].dot(translation);
     }
+}
 
-    return ret;
+void Transform::translate(const real_t x, const real_t y, const real_t z) {
+    translate(Vector3{x, y, z});
+}
+
+Transform Transform::translated(const Vector3& translation) const {
+    Transform result = *this;
+    result.translate(translation);
+    return result;
+}
+
+void Transform::rotate(const Vector3& axis, const real_t radians) {
+    *this = rotated(axis, radians);
+}
+
+Transform Transform::rotated(const Vector3& axis, const real_t radians) const {
+    return Transform(Basis(axis, radians), Vector3()) * *this;
+}
+
+void Transform::rotate_basis(const Vector3& axis, const real_t radians) {
+    basis.rotate(axis, radians);
+}
+
+void Transform::scale(const Vector3& factor) {
+    basis.scale(factor);
+    origin *= factor;
+}
+
+Transform Transform::scaled(const Vector3& factor) const {
+    Transform result = *this;
+    result.scale(factor);
+    return result;
+}
+
+void Transform::scale_basis(const Vector3& factor) {
+    basis.scale(factor);
+}
+
+Transform Transform::looking_at(const Vector3& target, const Vector3& up)
+    const {
+    Transform result = *this;
+    result.set_look_at(origin, target, up);
+    return result;
+}
+
+void Transform::set_look_at(
+    const Vector3& eye,
+    const Vector3& target,
+    const Vector3& up
+) {
+    const Vector3 z_vector = (eye - target).normalized();
+    const Vector3 x_vector = up.cross(z_vector).normalized();
+    const Vector3 y_vector = z_vector.cross(x_vector).normalized();
+    basis.set_axis(0, x_vector);
+    basis.set_axis(1, y_vector);
+    basis.set_axis(2, z_vector);
+    origin = eye;
+}
+
+void Transform::invert() {
+    affine_invert();
+}
+
+Transform Transform::inverse() const {
+    Transform result = *this;
+    result.invert();
+    return result;
 }
 
 void Transform::affine_invert() {
@@ -178,124 +165,9 @@ void Transform::affine_invert() {
 }
 
 Transform Transform::affine_inverse() const {
-    Transform ret = *this;
-    ret.affine_invert();
-    return ret;
-}
-
-void Transform::invert() {
-    basis.transpose();
-    origin = basis.xform(-origin);
-}
-
-Transform Transform::inverse() const {
-    // FIXME: this function assumes the basis is a rotation matrix, with no
-    // scaling. Transform::affine_inverse can handle matrices with scaling, so
-    // GDScript should eventually use that.
-    Transform ret = *this;
-    ret.invert();
-    return ret;
-}
-
-void Transform::rotate(const Vector3& p_axis, real_t p_phi) {
-    *this = rotated(p_axis, p_phi);
-}
-
-Transform Transform::rotated(const Vector3& p_axis, real_t p_phi) const {
-    return Transform(Basis(p_axis, p_phi), Vector3()) * (*this);
-}
-
-void Transform::rotate_basis(const Vector3& p_axis, real_t p_phi) {
-    basis.rotate(p_axis, p_phi);
-}
-
-Transform Transform::looking_at(const Vector3& p_target, const Vector3& p_up)
-    const {
-    Transform t = *this;
-    t.set_look_at(origin, p_target, p_up);
-    return t;
-}
-
-void Transform::set_look_at(
-    const Vector3& p_eye,
-    const Vector3& p_target,
-    const Vector3& p_up
-) {
-    // Reference: MESA source code
-    Vector3 v_x, v_y, v_z;
-
-    /* Make rotation matrix */
-
-    /* Z vector */
-    v_z = p_eye - p_target;
-
-    v_z.normalize();
-
-    v_y = p_up;
-
-    v_x = v_y.cross(v_z);
-
-    /* Recompute Y = Z cross X */
-    v_y = v_z.cross(v_x);
-
-    v_x.normalize();
-    v_y.normalize();
-
-    basis.set_axis(0, v_x);
-    basis.set_axis(1, v_y);
-    basis.set_axis(2, v_z);
-    origin = p_eye;
-}
-
-Transform Transform::interpolate_with(const Transform& p_transform, real_t p_c)
-    const {
-    /* not sure if very "efficient" but good enough? */
-
-    Vector3 src_scale = basis.get_scale();
-    Quat src_rot      = basis;
-    Vector3 src_loc   = origin;
-
-    Vector3 dst_scale = p_transform.basis.get_scale();
-    Quat dst_rot      = p_transform.basis;
-    Vector3 dst_loc   = p_transform.origin;
-
-    Transform dst;
-    dst.basis = src_rot.slerp(dst_rot, p_c);
-    dst.basis.scale(src_scale.linear_interpolate(dst_scale, p_c));
-    dst.origin = src_loc.linear_interpolate(dst_loc, p_c);
-
-    return dst;
-}
-
-void Transform::scale(const Vector3& p_scale) {
-    basis.scale(p_scale);
-    origin *= p_scale;
-}
-
-Transform Transform::scaled(const Vector3& p_scale) const {
-    Transform t = *this;
-    t.scale(p_scale);
-    return t;
-}
-
-void Transform::scale_basis(const Vector3& p_scale) {
-    basis.scale(p_scale);
-}
-
-void Transform::translate(real_t p_tx, real_t p_ty, real_t p_tz) {
-    translate(Vector3(p_tx, p_ty, p_tz));
-}
-
-void Transform::translate(const Vector3& p_translation) {
-    for (int i = 0; i < 3; i++) {
-        origin[i] += basis.elements[i].dot(p_translation);
-    }
-}
-
-Transform Transform::translated(const Vector3& p_translation) const {
-    Transform t = *this;
-    t.translate(p_translation);
-    return t;
+    Transform result = *this;
+    result.affine_invert();
+    return result;
 }
 
 void Transform::orthonormalize() {
@@ -303,37 +175,149 @@ void Transform::orthonormalize() {
 }
 
 Transform Transform::orthonormalized() const {
-    Transform _copy = *this;
-    _copy.orthonormalize();
-    return _copy;
+    Transform result = *this;
+    result.orthonormalize();
+    return result;
 }
 
-bool Transform::operator==(const Transform& p_transform) const {
-    return (basis == p_transform.basis && origin == p_transform.origin);
+Vector3 Transform::xform(const Vector3& vector3) const {
+    return {
+        basis.elements[0].dot(vector3) + origin.x,
+        basis.elements[1].dot(vector3) + origin.y,
+        basis.elements[2].dot(vector3) + origin.z
+    };
 }
 
-bool Transform::operator!=(const Transform& p_transform) const {
-    return (basis != p_transform.basis || origin != p_transform.origin);
+Vector3 Transform::xform_inv(const Vector3& vector3) const {
+    const Vector3 inverse_vector = vector3 - origin;
+    return {
+        basis.elements[0][0] * inverse_vector.x
+            + basis.elements[1][0] * inverse_vector.y
+            + basis.elements[2][0] * inverse_vector.z,
+        basis.elements[0][1] * inverse_vector.x
+            + basis.elements[1][1] * inverse_vector.y
+            + basis.elements[2][1] * inverse_vector.z,
+        basis.elements[0][2] * inverse_vector.x
+            + basis.elements[1][2] * inverse_vector.y
+            + basis.elements[2][2] * inverse_vector.z
+    };
 }
 
-void Transform::operator*=(const Transform& p_transform) {
-    origin  = xform(p_transform.origin);
-    basis  *= p_transform.basis;
+Plane Transform::xform(const Plane& plane) const {
+    const Vector3 point     = xform(plane.normal * plane.d);
+    const Vector3 direction = xform(point + plane.normal);
+    const Vector3 normal    = (direction - point).normalized();
+    const real_t distance   = normal.dot(point);
+    return {normal, distance};
 }
 
-Transform Transform::operator*(const Transform& p_transform) const {
-    Transform t  = *this;
-    t           *= p_transform;
-    return t;
+Plane Transform::xform_inv(const Plane& plane) const {
+    const Vector3 point     = xform_inv(plane.normal * plane.d);
+    const Vector3 direction = xform_inv(point + plane.normal);
+    const Vector3 normal    = (direction - point).normalized();
+    const real_t distance   = normal.dot(point);
+    return {normal, distance};
 }
 
-Transform::operator String() const {
-    return basis.operator String() + " - " + origin.operator String();
+AABB Transform::xform(const AABB& aabb) const {
+    const Vector3 position = xform(aabb.position);
+    const Vector3 x_length = basis.get_axis(0) * aabb.size.x;
+    const Vector3 y_length = basis.get_axis(1) * aabb.size.y;
+    const Vector3 z_length = basis.get_axis(2) * aabb.size.z;
+    AABB result;
+    result.position = position;
+    result.expand_to(position + x_length);
+    result.expand_to(position + y_length);
+    result.expand_to(position + z_length);
+    result.expand_to(position + x_length + y_length);
+    result.expand_to(position + x_length + z_length);
+    result.expand_to(position + y_length + z_length);
+    result.expand_to(position + x_length + y_length + z_length);
+    return result;
 }
 
-Transform::Transform(const Basis& p_basis, const Vector3& p_origin) {
-    basis  = p_basis;
-    origin = p_origin;
+AABB Transform::xform_inv(const AABB& aabb) const {
+    const Vector3 vertices[8] = {
+        Vector3{
+                aabb.position.x + aabb.size.x,
+                aabb.position.y + aabb.size.y,
+                aabb.position.z + aabb.size.z
+        },
+        Vector3{
+                aabb.position.x + aabb.size.x,
+                aabb.position.y + aabb.size.y,
+                aabb.position.z
+        },
+        Vector3{
+                aabb.position.x + aabb.size.x,
+                aabb.position.y,
+                aabb.position.z + aabb.size.z
+        },
+        Vector3{
+                aabb.position.x + aabb.size.x,
+                aabb.position.y,
+                aabb.position.z
+        },
+        Vector3{
+                aabb.position.x,
+                aabb.position.y + aabb.size.y,
+                aabb.position.z + aabb.size.z
+        },
+        Vector3{
+                aabb.position.x,
+                aabb.position.y + aabb.size.y,
+                aabb.position.z
+        },
+        Vector3{
+                aabb.position.x,
+                aabb.position.y,
+                aabb.position.z + aabb.size.z
+        },
+        Vector3{aabb.position.x,               aabb.position.y, aabb.position.z}
+    };
+
+    AABB result;
+    result.position = xform_inv(vertices[0]);
+    for (int i = 1; i < 8; i++) {
+        result.expand_to(xform_inv(vertices[i]));
+    }
+    return result;
 }
 
+Transform Transform::inverse_xform(const Transform& other) const {
+    const Vector3 translation = other.origin - origin;
+    return {basis.transpose_xform(other.basis), basis.xform(translation)};
+}
+
+Transform Transform::interpolate_with(const Transform& to, const real_t factor)
+    const {
+    const Vector3 from_position = origin;
+    const Quat from_rotation    = basis;
+    const Vector3 from_scale    = basis.get_scale();
+    const Vector3 to_position   = to.origin;
+    const Quat to_rotation      = to.basis;
+    const Vector3 to_scale      = to.basis.get_scale();
+    Transform result;
+    result.basis = from_rotation.slerp(to_rotation, factor);
+    result.basis.scale(from_scale.linear_interpolate(to_scale, factor));
+    result.origin = from_position.linear_interpolate(to_position, factor);
+    return result;
+}
+
+bool operator==(const Transform& left, const Transform& right) {
+    return left.basis == right.basis && left.origin == right.origin;
+}
+
+bool operator!=(const Transform& left, const Transform& right) {
+    return !(left == right);
+}
+
+Transform operator*(Transform left, const Transform& right) {
+    left *= right;
+    return right;
+}
+
+Vector3 operator*(const Transform& transform, const Vector3& vector3) {
+    return transform.xform(vector3);
+}
 } // namespace Rebel
