b4bef7a3daf808e2a9f2ddb08b86a9e44bc9e755
Marcel Admiraal <madmiraal@users.noreply.github.com>
2025-12-13 12:02:20 +0100

    Clean up Rect2

--- include/common/rect2.h
+++ include/common/rect2.h
@@ -9,170 +9,56 @@
 
 #include "common/vector2.h"
 
-#include <cmath>
-#include <cstdlib>
-
 namespace Rebel {
-
 class String;
-
-typedef Vector2 Size2;
+class Transform2D;
 typedef Vector2 Point2;
+typedef Vector2 Size2;
 
-struct Transform2D;
-
-struct Rect2 {
+class Rect2 {
+public:
     Point2 position;
     Size2 size;
 
-    inline const Vector2& get_position() const {
-        return position;
-    }
-
-    inline void set_position(const Vector2& p_position) {
-        position = p_position;
-    }
-
-    inline const Vector2& get_size() const {
-        return size;
-    }
-
-    inline void set_size(const Vector2& p_size) {
-        size = p_size;
-    }
-
-    inline real_t get_area() const {
-        return size.width * size.height;
-    }
-
-    inline bool intersects(const Rect2& p_rect) const {
-        if (position.x >= (p_rect.position.x + p_rect.size.width)) {
-            return false;
-        }
-        if ((position.x + size.width) <= p_rect.position.x) {
-            return false;
-        }
-        if (position.y >= (p_rect.position.y + p_rect.size.height)) {
-            return false;
-        }
-        if ((position.y + size.height) <= p_rect.position.y) {
-            return false;
-        }
-
-        return true;
-    }
-
-    real_t distance_to(const Vector2& p_point) const;
-
-    bool intersects_transformed(const Transform2D& p_xform, const Rect2& p_rect)
-        const;
-
-    bool intersects_segment(
-        const Point2& p_from,
-        const Point2& p_to,
-        Point2* r_position = nullptr,
-        Point2* r_normal   = nullptr
-    ) const;
-
-    inline bool encloses(const Rect2& p_rect) const {
-        return (p_rect.position.x >= position.x)
-            && (p_rect.position.y >= position.y)
-            && ((p_rect.position.x + p_rect.size.x) < (position.x + size.x))
-            && ((p_rect.position.y + p_rect.size.y) < (position.y + size.y));
-    }
-
-    inline bool has_no_area() const {
-        return (size.x <= 0 || size.y <= 0);
-    }
-
-    Rect2 clip(const Rect2& p_rect) const;
-
-    Rect2 merge(const Rect2& p_rect) const;
-
-    inline bool has_point(const Point2& p_point) const {
-        if (p_point.x < position.x) {
-            return false;
-        }
-        if (p_point.y < position.y) {
-            return false;
-        }
-
-        if (p_point.x >= (position.x + size.x)) {
-            return false;
-        }
-        if (p_point.y >= (position.y + size.y)) {
-            return false;
-        }
-
-        return true;
-    }
-
-    inline bool no_area() const {
-        return (size.width <= 0 || size.height <= 0);
-    }
-
-    inline bool operator==(const Rect2& p_rect) const {
-        return position == p_rect.position && size == p_rect.size;
-    }
-
-    inline bool operator!=(const Rect2& p_rect) const {
-        return position != p_rect.position || size != p_rect.size;
-    }
-
-    inline Rect2 grow(real_t p_by) const {
-        Rect2 g        = *this;
-        g.position.x  -= p_by;
-        g.position.y  -= p_by;
-        g.size.width  += p_by * 2;
-        g.size.height += p_by * 2;
-        return g;
-    }
-
-    inline Rect2 expand(const Vector2& p_vector) const {
-        Rect2 r = *this;
-        r.expand_to(p_vector);
-        return r;
-    }
-
-    inline void expand_to(const Vector2& p_vector
-    ) { // in place function for speed
-
-        Vector2 begin = position;
-        Vector2 end   = position + size;
-
-        if (p_vector.x < begin.x) {
-            begin.x = p_vector.x;
-        }
-        if (p_vector.y < begin.y) {
-            begin.y = p_vector.y;
-        }
-
-        if (p_vector.x > end.x) {
-            end.x = p_vector.x;
-        }
-        if (p_vector.y > end.y) {
-            end.y = p_vector.y;
-        }
-
-        position = begin;
-        size     = end - begin;
-    }
+    Rect2() = default;
+    Rect2(const Point2& position, const Size2& size);
+    Rect2(real_t x, real_t y, real_t width, real_t height);
 
     operator String() const;
 
-    inline Rect2() {}
+    const Point2& get_position() const;
+    void set_position(const Point2& new_position);
+    const Size2& get_size() const;
+    void set_size(const Size2& new_size);
 
-    inline Rect2(real_t p_x, real_t p_y, real_t p_width, real_t p_height) {
-        position = Point2(p_x, p_y);
-        size     = Size2(p_width, p_height);
-    }
+    real_t get_area() const;
+    real_t distance_to(const Point2& point) const;
 
-    inline Rect2(const Point2& p_position, const Size2& p_size) {
-        position = p_position;
-        size     = p_size;
-    }
+    Rect2 grow(real_t amount) const;
+    Rect2 expand(const Point2& point) const;
+    void expand_to(const Point2& point);
+    Rect2 clip(const Rect2& other) const;
+    Rect2 merge(const Rect2& other) const;
+
+    bool has_no_area() const;
+    bool no_area() const;
+    bool has_point(const Point2& point) const;
+    bool encloses(const Rect2& other) const;
+    bool intersects(const Rect2& other) const;
+    bool intersects_transformed(
+        const Transform2D& transform,
+        const Rect2& other
+    ) const;
+    bool intersects_segment(
+        const Point2& from,
+        const Point2& to,
+        Point2* r_position = nullptr,
+        Vector2* r_normal  = nullptr
+    ) const;
 };
 
+bool operator==(const Rect2& left, const Rect2& right);
+bool operator!=(const Rect2& left, const Rect2& right);
 } // namespace Rebel
 
 #endif // RECT2_H
--- src/common/rect2.cpp
+++ src/common/rect2.cpp
@@ -6,330 +6,342 @@
 
 #include "common/rect2.h"
 
+#include "common/rebelmath.h"
 #include "common/string.h"
 #include "common/transform2d.h"
 #include "common/vector2.h"
 
-#include <cmath>
-
 namespace Rebel {
+Rect2::Rect2(const Point2& position, const Size2& size) :
+    position(position),
+    size(size) {}
 
-#ifndef MAX
-#define MAX(a, b) (a > b ? a : b)
-#endif
-
-#ifndef MIN
-#define MIN(a, b) (a < b ? a : b)
-#endif
-
-real_t Rect2::distance_to(const Vector2& p_point) const {
-    real_t dist = 1e20f;
-
-    if (p_point.x < position.x) {
-        dist = MIN(dist, position.x - p_point.x);
-    }
-    if (p_point.y < position.y) {
-        dist = MIN(dist, position.y - p_point.y);
-    }
-    if (p_point.x >= (position.x + size.x)) {
-        dist = MIN(p_point.x - (position.x + size.x), dist);
-    }
-    if (p_point.y >= (position.y + size.y)) {
-        dist = MIN(p_point.y - (position.y + size.y), dist);
-    }
-
-    if (dist == 1e20f) {
-        return 0;
-    } else {
-        return dist;
-    }
-}
-
-Rect2 Rect2::clip(const Rect2& p_rect) const { /// return a clipped rect
-
-    Rect2 new_rect = p_rect;
-
-    if (!intersects(new_rect)) {
-        return Rect2();
-    }
-
-    new_rect.position.x = MAX(p_rect.position.x, position.x);
-    new_rect.position.y = MAX(p_rect.position.y, position.y);
-
-    Point2 p_rect_end = p_rect.position + p_rect.size;
-    Point2 end        = position + size;
-
-    new_rect.size.x = MIN(p_rect_end.x, end.x) - new_rect.position.x;
-    new_rect.size.y = MIN(p_rect_end.y, end.y) - new_rect.position.y;
-
-    return new_rect;
-}
-
-Rect2 Rect2::merge(const Rect2& p_rect) const { ///< return a merged rect
-
-    Rect2 new_rect;
-
-    new_rect.position.x = MIN(p_rect.position.x, position.x);
-    new_rect.position.y = MIN(p_rect.position.y, position.y);
-
-    new_rect.size.x =
-        MAX(p_rect.position.x + p_rect.size.x, position.x + size.x);
-    new_rect.size.y =
-        MAX(p_rect.position.y + p_rect.size.y, position.y + size.y);
-
-    new_rect.size = new_rect.size - new_rect.position; // make relative again
-
-    return new_rect;
-}
+Rect2::Rect2(
+    const real_t x,
+    const real_t y,
+    const real_t width,
+    const real_t height
+) :
+    position{x, y},
+    size{width, height} {}
 
 Rect2::operator String() const {
     return String(position) + ", " + String(size);
 }
 
-bool Rect2::intersects_segment(
-    const Point2& p_from,
-    const Point2& p_to,
-    Point2* r_position,
-    Point2* r_normal
-) const {
-    real_t min = 0, max = 1;
-    int axis    = 0;
-    real_t sign = 0;
+const Point2& Rect2::get_position() const {
+    return position;
+}
 
-    for (int i = 0; i < 2; i++) {
-        real_t seg_from  = p_from[i];
-        real_t seg_to    = p_to[i];
-        real_t box_begin = position[i];
-        real_t box_end   = box_begin + size[i];
-        real_t cmin, cmax;
-        real_t csign;
+void Rect2::set_position(const Point2& new_position) {
+    position = new_position;
+}
 
-        if (seg_from < seg_to) {
-            if (seg_from > box_end || seg_to < box_begin) {
-                return false;
-            }
-            real_t length = seg_to - seg_from;
-            cmin =
-                (seg_from < box_begin) ? ((box_begin - seg_from) / length) : 0;
-            cmax  = (seg_to > box_end) ? ((box_end - seg_from) / length) : 1;
-            csign = -1.0;
+const Size2& Rect2::get_size() const {
+    return size;
+}
 
-        } else {
-            if (seg_to > box_end || seg_from < box_begin) {
-                return false;
-            }
-            real_t length = seg_to - seg_from;
-            cmin  = (seg_from > box_end) ? (box_end - seg_from) / length : 0;
-            cmax  = (seg_to < box_begin) ? (box_begin - seg_from) / length : 1;
-            csign = 1.0;
-        }
+void Rect2::set_size(const Size2& new_size) {
+    size = new_size;
+}
 
-        if (cmin > min) {
-            min  = cmin;
-            axis = i;
-            sign = csign;
-        }
-        if (cmax < max) {
-            max = cmax;
-        }
-        if (max < min) {
-            return false;
-        }
+real_t Rect2::get_area() const {
+    return size.width * size.height;
+}
+
+real_t Rect2::distance_to(const Point2& point) const {
+    real_t distance = 1e20f;
+    if (point.x < position.x) {
+        distance = Math::min(distance, position.x - point.x);
     }
-
-    Vector2 rel = p_to - p_from;
-
-    if (r_normal) {
-        Vector2 normal;
-        normal[axis] = sign;
-        *r_normal    = normal;
+    if (point.y < position.y) {
+        distance = Math::min(distance, position.y - point.y);
     }
-
-    if (r_position) {
-        *r_position = p_from + rel * min;
+    if (point.x >= position.x + size.x) {
+        distance = Math::min(point.x - (position.x + size.x), distance);
     }
+    if (point.y >= position.y + size.y) {
+        distance = Math::min(point.y - (position.y + size.y), distance);
+    }
+    if (distance == 1e20f) {
+        return 0;
+    }
+    return distance;
+}
 
+Rect2 Rect2::grow(const real_t amount) const {
+    Rect2 result        = *this;
+    result.position.x  -= amount;
+    result.position.y  -= amount;
+    result.size.width  += amount * 2;
+    result.size.height += amount * 2;
+    return result;
+}
+
+Rect2 Rect2::expand(const Vector2& point) const {
+    Rect2 result = *this;
+    result.expand_to(point);
+    return result;
+}
+
+void Rect2::expand_to(const Point2& point) {
+    Vector2 begin = position;
+    Vector2 end   = position + size;
+    if (point.x < begin.x) {
+        begin.x = point.x;
+    }
+    if (point.y < begin.y) {
+        begin.y = point.y;
+    }
+    if (point.x > end.x) {
+        end.x = point.x;
+    }
+    if (point.y > end.y) {
+        end.y = point.y;
+    }
+    position = begin;
+    size     = end - begin;
+}
+
+Rect2 Rect2::clip(const Rect2& other) const {
+    if (!intersects(other)) {
+        return {};
+    }
+    const real_t minimum_x = Math::max(other.position.x, position.x);
+    const real_t minimum_y = Math::max(other.position.y, position.y);
+    const real_t maximum_x =
+        Math::min(other.position.x + other.size.width, position.x + size.width);
+    const real_t maximum_y = Math::min(
+        other.position.y + other.size.height,
+        position.y + size.height
+    );
+    return {minimum_x, minimum_y, maximum_x - minimum_x, maximum_y - minimum_y};
+}
+
+Rect2 Rect2::merge(const Rect2& other) const {
+    const real_t minimum_x = Math::min(other.position.x, position.x);
+    const real_t minimum_y = Math::min(other.position.y, position.y);
+    const real_t maximum_x =
+        Math::max(other.position.x + other.size.width, position.x + size.width);
+    const real_t maximum_y = Math::max(
+        other.position.y + other.size.height,
+        position.y + size.height
+    );
+    return {minimum_x, minimum_y, maximum_x - minimum_x, maximum_y - minimum_y};
+}
+
+bool Rect2::has_no_area() const {
+    return size.x <= 0 || size.y <= 0;
+}
+
+bool Rect2::no_area() const {
+    return has_no_area();
+}
+
+bool Rect2::has_point(const Point2& point) const {
+    if (point.x < position.x) {
+        return false;
+    }
+    if (point.y < position.y) {
+        return false;
+    }
+    if (point.x >= position.x + size.x) {
+        return false;
+    }
+    if (point.y >= position.y + size.y) {
+        return false;
+    }
+    return true;
+}
+
+bool Rect2::encloses(const Rect2& other) const {
+    return other.position.x >= position.x && other.position.y >= position.y
+        && other.position.x + other.size.x < position.x + size.x
+        && other.position.y + other.size.y < position.y + size.y;
+}
+
+bool Rect2::intersects(const Rect2& other) const {
+    if (position.x >= other.position.x + other.size.width) {
+        return false;
+    }
+    if (position.x + size.width <= other.position.x) {
+        return false;
+    }
+    if (position.y >= other.position.y + other.size.height) {
+        return false;
+    }
+    if (position.y + size.height <= other.position.y) {
+        return false;
+    }
     return true;
 }
 
 bool Rect2::intersects_transformed(
-    const Transform2D& p_xform,
-    const Rect2& p_rect
+    const Transform2D& transform,
+    const Rect2& other
 ) const {
-    // SAT intersection between local and transformed rect2
-
-    Vector2 xf_points[4] = {
-        p_xform.xform(p_rect.position),
-        p_xform.xform(
-            Vector2(p_rect.position.x + p_rect.size.x, p_rect.position.y)
+    const Vector2 other_points[4] = {
+        transform.xform(other.position),
+        transform.xform(
+            Vector2{other.position.x + other.size.x, other.position.y}
         ),
-        p_xform.xform(
-            Vector2(p_rect.position.x, p_rect.position.y + p_rect.size.y)
+        transform.xform(
+            Vector2{other.position.x, other.position.y + other.size.y}
         ),
-        p_xform.xform(Vector2(
-            p_rect.position.x + p_rect.size.x,
-            p_rect.position.y + p_rect.size.y
-        )),
+        transform.xform(Vector2{
+            other.position.x + other.size.x,
+            other.position.y + other.size.y
+        }),
     };
-
-    real_t low_limit;
-
-    // base rect2 first (faster)
-
-    if (xf_points[0].y > position.y) {
-        goto next1;
-    }
-    if (xf_points[1].y > position.y) {
-        goto next1;
-    }
-    if (xf_points[2].y > position.y) {
-        goto next1;
-    }
-    if (xf_points[3].y > position.y) {
-        goto next1;
+    if (!(has_point(other_points[0]) || has_point(other_points[1])
+          || has_point(other_points[2]) || has_point(other_points[3]))) {
+        return false;
     }
 
-    return false;
-
-next1:
-
-    low_limit = position.y + size.y;
-
-    if (xf_points[0].y < low_limit) {
-        goto next2;
-    }
-    if (xf_points[1].y < low_limit) {
-        goto next2;
-    }
-    if (xf_points[2].y < low_limit) {
-        goto next2;
-    }
-    if (xf_points[3].y < low_limit) {
-        goto next2;
-    }
-
-    return false;
-
-next2:
-
-    if (xf_points[0].x > position.x) {
-        goto next3;
-    }
-    if (xf_points[1].x > position.x) {
-        goto next3;
-    }
-    if (xf_points[2].x > position.x) {
-        goto next3;
-    }
-    if (xf_points[3].x > position.x) {
-        goto next3;
-    }
-
-    return false;
-
-next3:
-
-    low_limit = position.x + size.x;
-
-    if (xf_points[0].x < low_limit) {
-        goto next4;
-    }
-    if (xf_points[1].x < low_limit) {
-        goto next4;
-    }
-    if (xf_points[2].x < low_limit) {
-        goto next4;
-    }
-    if (xf_points[3].x < low_limit) {
-        goto next4;
-    }
-
-    return false;
-
-next4:
-
-    Vector2 xf_points2[4] = {
+    const Vector2 points[4] = {
         position,
         Vector2(position.x + size.x, position.y),
         Vector2(position.x, position.y + size.y),
         Vector2(position.x + size.x, position.y + size.y),
     };
 
-    real_t maxa = p_xform.elements[0].dot(xf_points2[0]);
-    real_t mina = maxa;
+    const real_t this_dp00 = transform.elements[0].dot(points[0]);
+    real_t this_maximum0   = this_dp00;
+    real_t this_minimum0   = this_dp00;
+    const real_t this_dp01 = transform.elements[0].dot(points[1]);
+    this_maximum0          = Math::max(this_dp01, this_maximum0);
+    this_minimum0          = Math::min(this_dp01, this_minimum0);
+    const real_t this_dp02 = transform.elements[0].dot(points[2]);
+    this_maximum0          = Math::max(this_dp02, this_maximum0);
+    this_minimum0          = Math::min(this_dp02, this_minimum0);
+    const real_t this_dp03 = transform.elements[0].dot(points[3]);
+    this_maximum0          = Math::max(this_dp03, this_maximum0);
+    this_minimum0          = Math::min(this_dp03, this_minimum0);
 
-    real_t dp = p_xform.elements[0].dot(xf_points2[1]);
-    maxa      = MAX(dp, maxa);
-    mina      = MIN(dp, mina);
+    const real_t other_dp00 = transform.elements[0].dot(other_points[0]);
+    real_t other_maximum0   = other_dp00;
+    real_t other_minimum0   = other_dp00;
+    const real_t other_dp01 = transform.elements[0].dot(other_points[1]);
+    other_maximum0          = Math::max(other_dp01, other_maximum0);
+    other_minimum0          = Math::min(other_dp01, other_minimum0);
+    const real_t other_dp02 = transform.elements[0].dot(other_points[2]);
+    other_maximum0          = Math::max(other_dp02, other_maximum0);
+    other_minimum0          = Math::min(other_dp02, other_minimum0);
+    const real_t other_dp03 = transform.elements[0].dot(other_points[3]);
+    other_maximum0          = Math::max(other_dp03, other_maximum0);
+    other_minimum0          = Math::min(other_dp03, other_minimum0);
 
-    dp   = p_xform.elements[0].dot(xf_points2[2]);
-    maxa = MAX(dp, maxa);
-    mina = MIN(dp, mina);
-
-    dp   = p_xform.elements[0].dot(xf_points2[3]);
-    maxa = MAX(dp, maxa);
-    mina = MIN(dp, mina);
-
-    real_t maxb = p_xform.elements[0].dot(xf_points[0]);
-    real_t minb = maxb;
-
-    dp   = p_xform.elements[0].dot(xf_points[1]);
-    maxb = MAX(dp, maxb);
-    minb = MIN(dp, minb);
-
-    dp   = p_xform.elements[0].dot(xf_points[2]);
-    maxb = MAX(dp, maxb);
-    minb = MIN(dp, minb);
-
-    dp   = p_xform.elements[0].dot(xf_points[3]);
-    maxb = MAX(dp, maxb);
-    minb = MIN(dp, minb);
-
-    if (mina > maxb) {
-        return false;
-    }
-    if (minb > maxa) {
+    if (this_minimum0 > other_maximum0 || other_minimum0 > this_maximum0) {
         return false;
     }
 
-    maxa = p_xform.elements[1].dot(xf_points2[0]);
-    mina = maxa;
+    const real_t this_dp10 = transform.elements[1].dot(points[0]);
+    real_t this_maximum1   = this_dp10;
+    real_t this_minimum1   = this_dp10;
+    const real_t this_dp11 = transform.elements[1].dot(points[1]);
+    this_maximum1          = Math::max(this_dp11, this_maximum1);
+    this_minimum1          = Math::min(this_dp11, this_minimum1);
+    const real_t this_dp12 = transform.elements[1].dot(points[2]);
+    this_maximum1          = Math::max(this_dp12, this_maximum1);
+    this_minimum1          = Math::min(this_dp12, this_minimum1);
+    const real_t this_dp13 = transform.elements[1].dot(points[3]);
+    this_maximum1          = Math::max(this_dp13, this_maximum1);
+    this_minimum1          = Math::min(this_dp13, this_minimum1);
 
-    dp   = p_xform.elements[1].dot(xf_points2[1]);
-    maxa = MAX(dp, maxa);
-    mina = MIN(dp, mina);
+    const real_t other_dp10 = transform.elements[1].dot(other_points[0]);
+    real_t other_maximum1   = other_dp10;
+    real_t other_minimum1   = other_dp10;
+    const real_t other_dp11 = transform.elements[1].dot(other_points[1]);
+    other_maximum1          = Math::max(other_dp11, other_maximum1);
+    other_minimum1          = Math::min(other_dp11, other_minimum1);
+    const real_t other_dp12 = transform.elements[1].dot(other_points[2]);
+    other_maximum1          = Math::max(other_dp12, other_maximum1);
+    other_minimum1          = Math::min(other_dp12, other_minimum1);
+    const real_t other_dp13 = transform.elements[1].dot(other_points[3]);
+    other_maximum1          = Math::max(other_dp13, other_maximum1);
+    other_minimum1          = Math::min(other_dp13, other_minimum1);
 
-    dp   = p_xform.elements[1].dot(xf_points2[2]);
-    maxa = MAX(dp, maxa);
-    mina = MIN(dp, mina);
-
-    dp   = p_xform.elements[1].dot(xf_points2[3]);
-    maxa = MAX(dp, maxa);
-    mina = MIN(dp, mina);
-
-    maxb = p_xform.elements[1].dot(xf_points[0]);
-    minb = maxb;
-
-    dp   = p_xform.elements[1].dot(xf_points[1]);
-    maxb = MAX(dp, maxb);
-    minb = MIN(dp, minb);
-
-    dp   = p_xform.elements[1].dot(xf_points[2]);
-    maxb = MAX(dp, maxb);
-    minb = MIN(dp, minb);
-
-    dp   = p_xform.elements[1].dot(xf_points[3]);
-    maxb = MAX(dp, maxb);
-    minb = MIN(dp, minb);
-
-    if (mina > maxb) {
-        return false;
-    }
-    if (minb > maxa) {
+    if (this_minimum1 > other_maximum1 || other_minimum1 > this_maximum1) {
         return false;
     }
 
     return true;
 }
 
+bool Rect2::intersects_segment(
+    const Point2& from,
+    const Point2& to,
+    Point2* r_position,
+    Vector2* r_normal
+) const {
+    real_t minimum        = 0;
+    real_t maximum        = 1;
+    int intersection_axis = 0;
+    real_t normal_sign    = 0;
+
+    for (int axis = 0; axis < 2; axis++) {
+        const real_t box_begin = position[axis];
+        const real_t box_end   = box_begin + size[axis];
+        real_t axis_minimum    = 0;
+        real_t axis_maximum    = 1;
+        real_t axis_sign;
+
+        if (from[axis] < to[axis]) {
+            if (from[axis] > box_end || to[axis] < box_begin) {
+                return false;
+            }
+            const real_t length = to[axis] - from[axis];
+            if (from[axis] < box_begin) {
+                axis_minimum = (box_begin - from[axis]) / length;
+            }
+            if (to[axis] > box_end) {
+                axis_maximum = (box_end - from[axis]) / length;
+            }
+            axis_sign = -1.0;
+        } else {
+            if (to[axis] > box_end || from[axis] < box_begin) {
+                return false;
+            }
+            const real_t length = to[axis] - from[axis];
+            if (from[axis] > box_end) {
+                axis_minimum = (box_end - from[axis]) / length;
+            }
+            if (to[axis] < box_begin) {
+                axis_maximum = (box_begin - from[axis]) / length;
+            }
+            axis_sign = 1.0;
+        }
+
+        if (axis_minimum > minimum) {
+            minimum           = axis_minimum;
+            intersection_axis = axis;
+            normal_sign       = axis_sign;
+        }
+        if (axis_maximum < maximum) {
+            maximum = axis_maximum;
+        }
+        if (maximum < minimum) {
+            return false;
+        }
+    }
+
+    const Vector2 direction = to - from;
+    if (r_normal) {
+        *r_normal                      = Vector2{};
+        (*r_normal)[intersection_axis] = normal_sign;
+    }
+    if (r_position) {
+        *r_position = from + direction * minimum;
+    }
+
+    return true;
+}
+
+bool operator==(const Rect2& left, const Rect2& right) {
+    return left.position == right.position && left.size == right.size;
+}
+
+bool operator!=(const Rect2& left, const Rect2& right) {
+    return !(left == right);
+}
 } // namespace Rebel
